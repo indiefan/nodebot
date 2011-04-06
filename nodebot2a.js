98b39f661a0112283a2c93cbcf390f8a630d02ee
indiefan <indiefan@gmail.com>
2011-04-06 09:35:18 -0700

    Added insult command

--- nodebot2a.js
+++ nodebot2a.js
@@ -31,3 +31,61 @@ nodebot2a.addMessageHandler(/nodebot2a\: leave/i, function(from, to, message) {
 		}
 	}
 });
+
+nodebot2a.addMessageHandler(/nodebot2a\: (insult)|(reprimand) \w*/, function(from, to, message) {
+	var adverbs = [
+		"incredibly",
+		"unusually",
+		"especially",
+		"extraordinarily"
+	];
+
+	var adjectives = [
+		"obnoxious",
+		"loud",
+		"ugly",
+		"ridiculous"
+	];
+
+	var nouns = [
+		"jerk",
+		"douche",
+		"asshole"
+	];
+
+	var request = message.split(" ");
+	var insultee = request[2];
+	if (insultee == "me") {
+		insultee = from;
+	}
+	
+	var rand1 = Math.floor(Math.random()*adverbs.length);
+	var rand2 = Math.floor(Math.random()*adjectives.length);
+	var rand3 = Math.floor(Math.random()*nouns.length);
+
+	var adverb = adverbs[rand1];
+	var adjective = adjectives[rand2];
+	var noun = nouns[rand3];
+
+	var article = 'a';
+	var vowels = ['a', 'e', 'i', 'o', 'u'];
+	if (inArray(adverb.substring(0,1), vowels)) {
+		article = 'an';
+	}
+
+	bot.say(to, insultee + ': You are ' + article + ' ' + adverb + ' ' + adjective + ' ' + noun);
+});
+
+
+//==========================================================================
+// Utility Methods
+//==========================================================================
+
+function inArray(needle, haystack) {
+    var length = haystack.length;
+    for(var i = 0; i < length; i++) {
+        if(haystack[i] == needle) return true;
+    }
+    return false;
+}
+
