14226f089070b9ca0d7a41190e328ad6be7541ae
indiefan <indiefan@gmail.com>
2011-04-06 09:14:03 -0700

    Resolved scope issue for bot, changing say commands back to direct calls.

--- nodebot2a.js
+++ nodebot2a.js
@@ -8,21 +8,21 @@ nodebot2a.addMessageHandler(/goodbye/i, function(from, to, message) {
 });
 
 nodebot2a.addMessageHandler(/hello/i, function(from, to, message) {
-	nodebot2a.say(to, 'Hello there ' + from);
+	bot.say(to, 'Hello there ' + from);
 });
 
 nodebot2a.addMessageHandler(/dance/, function(from, to, message) {
-	setTimeout(function () { nodebot2a.say(to, "\u0001ACTION dances: :D\\-<\u0001") }, 1000);
-	setTimeout(function () { nodebot2a.say(to, "\u0001ACTION dances: :D|-<\u0001")  }, 2000);
-	setTimeout(function () { nodebot2a.say(to, "\u0001ACTION dances: :D/-<\u0001")  }, 3000);
-	setTimeout(function () { nodebot2a.say(to, "\u0001ACTION dances: :D|-<\u0001")  }, 4000);
+	setTimeout(function () { bot.say(to, "\u0001ACTION dances: :D\\-<\u0001") }, 1000);
+	setTimeout(function () { bot.say(to, "\u0001ACTION dances: :D|-<\u0001")  }, 2000);
+	setTimeout(function () { bot.say(to, "\u0001ACTION dances: :D/-<\u0001")  }, 3000);
+	setTimeout(function () { bot.say(to, "\u0001ACTION dances: :D|-<\u0001")  }, 4000);
 });
 
 nodebot2a.addMessageHandler(/nodebot2a\: leave/i, function(from, to, message) {
 	var me = bot.nick;
 	if (from == "indiefan2a") {
 		console.log('Quitting...');
-		nodebot2a.bot.disconnect("Fine. I'll leave.");
+		bot.disconnect("Fine. I'll leave.");
 	} else {
 		if (from != "indiefan2a") {
 			bot.say(to, "You're not the boss of me, " + from);
